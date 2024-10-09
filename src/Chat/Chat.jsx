4454938974747c4b14cb48970dc7ee6a9cb5a951
Naimat Ansari <naimatansari31@gmail.com>
2024-10-10 00:10:33 +0500

    add

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
 const Chat = () => {
+
+  const example = [
+    'How to use Tailwind CSS',
+    'How to use Tailwind CSS with React JS',
+    'How to use Tailwind CSS with Next JS',
+    'How to use Tailwind CSS with Gatsby',
+    'How to use Tailwind CSS with Svelte',
+    'How to use Tailwind CSS with Vue JS',
+    'How to use Tailwind CSS with Angular JS',
+    'How to use Tailwind CSS with Ember',
+  ]
+
   return (
     <div className='h-screen w-screen bg-[#050509] flex'>
       <div className='w-[20%] h-screen bg-[#0c0c15] text-white p-4'>
@@ -22,13 +34,18 @@ const Chat = () => {
         <button className='py-3  px-6 rounded  mt-4 text-lg  flex justify-center text-center  items-center gap-1  hover:bg-slate-600 cursor-pointer'><i class='bx bx-cog'></i> Settings</button>
         </div>
       </div>
-      <div className='w-[80%] bg-[#2c2cf0] h-screen flex flex-col justify-center items-center text-white'>
-      <div className='h-[80%] border  '>
-        <div>APP GBT</div>
-        <div></div>
-        <div></div>
+      <div className='w-[80%]'>
+      <div className='h-[80%] border  flex flex-col  justify-center items-center text-white '>
+        <div className='text-4xl font-bold'>APP GBT</div>
+        <div className='flex flex-wrap justify-evenly   max-w-[600px]:'>
+          {
+            example.map((item,index)=>(
+              <div className='text-lg text-[16px] mt-3  p-2 font-light min-w-[400px] border rounded cursor-pointer hover:bg-slate-600'>{item}</div>
+            ))
+          }
+        </div>
       </div>
-      <div className='h-[20%] '></div>
+      <div className='h-[20%] border '></div>
       </div>
     </div>
   )
