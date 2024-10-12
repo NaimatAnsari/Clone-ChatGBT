c0d254ed3410b30875b39d5b0f0d794b01fa233a
Naimat Ansari <naimatansari31@gmail.com>
2024-10-12 23:39:06 +0500

    add.

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -14,6 +14,21 @@ const Chat = () => {
     'How to use Tailwind CSS with Ember',
   ]
 
+  const chats = [
+    {
+      role : 'user',
+      message: 'I want to Know how to use Tailwind CSS'
+    },
+    {
+      role : 'assistant',
+      message: 'Hi, I am ChatGBT. How can I help you?'
+    },
+    {
+      role : 'user',
+      message: 'Hi, I am ChatGBT. How can I help you?'
+    }
+  ]
+
   return (
     <div className='h-screen w-screen bg-[#050509] flex'>
       <div className='w-[20%] px-6 h-screen bg-[#0c0c15] text-white p-4'>
@@ -23,7 +38,7 @@ const Chat = () => {
         </div>
        
         <div className='flex items-center justify-start '>
-          <button className='w-full  h-[50px] text-white font-[600] rounded bg-[#4f4fe2] hover:bg-slate-600'>+ New Chat</button>
+          <button className='w-full  h-[50px] text-white font-[600] outline-none rounded bg-[#4f4fe2] hover:bg-slate-600'>+ New Chat</button>
         </div>
         <div className='h-[60%] mt-8 overflow-scroll overflow-x-hidden hide-scroll-bar shadow-lg'>
           {
@@ -41,22 +56,43 @@ const Chat = () => {
         </div>
       </div>
       <div className='w-[80%]'>
-      <div className='h-[80%]  flex flex-col  justify-center items-center text-white '>
+            {
+              chats.length > 0 ? 
+              (<div className='h-[80%] overflow-scroll hide-scroll-bar pt-8'>
+                {
+                  chats.map((item,index)=>{
+                    <div className='w-[60%] border mx-auto p-6 text-white'>
+                    <span className='text-white'>
+                      {
+                        item.role === 'user' ? <i className='bx bx-user'></i> : <i className='bx bxs-user'></i>
+                  }
+                  </span>
+                      <div>{item.message}</div>
+                    </div>
+                  })
+                }
+              </div>) 
+              :
+               (
+                <div className='h-[80%]  flex flex-col  justify-center items-center text-white '>
       
-        <div className='text-4xl font-bold'>APP GBT</div>
-        <div className='flex flex-wrap justify-evenly py-4  max-w-[900px]:'>
-          {
-            example.map((item,index)=>(
-              <div className='text-lg text-[16px] mt-3 p-2 font-light min-w-[400px] border rounded cursor-pointer hover:bg-slate-600'>{item}</div>
-            ))
-          }
-        </div>
-      </div>
+                <div className='text-4xl font-bold'>APP GBT</div>
+                <div className='flex flex-wrap justify-evenly py-4  max-w-[900px]:'>
+                  {
+                    example.map((item,index)=>(
+                      <div className='text-lg text-[16px] mt-3 p-2 font-light min-w-[400px] border rounded cursor-pointer hover:bg-slate-600'>{item}</div>
+                    ))
+                  }
+                </div>
+              </div>
+               )
+            }
+     
       <div className='h-[20%]  '>
         <div className='flex flex-col justify-center items-center w-full h-full'>
           <div className='flex justify-between w-[60%] bg-slate-800 rounded-lg items-center'>
           <input type="text" className=' w-[100%] rounded-lg p-4 bg-slate-800 text-white outline-none ' placeholder='Type your message here...'/>
-          <span><i className='bx bx-send pr-4 text-white text-2xl'></i></span>
+          <span><i className='bx bx-send mr-3 text-white text-2xl cursor-pointer'></i></span>
           </div>
           
           <small className='text-slate-500 mt-2 '>ChatGPT can make mistakes. Check important info.</small>
