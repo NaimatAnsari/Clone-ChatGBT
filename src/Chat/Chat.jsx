df6d1f45873818b5f12b501d49171d73cb75547d
Naimat Ansari <naimatansari31@gmail.com>
2024-10-13 13:56:09 +0500

    add

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -12,31 +12,18 @@ const Chat = () => {
     'How to use Tailwind CSS with Ember',
   ]
 
-  const chats = [
-    {
-      role: 'user',
-      message: 'I want to know how to use Tailwind CSS',
-    },
-    {
-      role: 'assistant',
-      message: 'Hi, I am ChatGBT. How can I help you?',
-    },
-    {
-      role: 'user',
-      message: 'Hi, I am ChatGBT. How can I help you?',
-    },
-  ]
+  const chats = []
 
   return (
     <div className='h-screen w-screen bg-[#050509] flex'>
       <div className='w-[20%] px-6 h-screen bg-[#0c0c15] text-white p-4'>
         <div className='flex items-center gap-2 justify-start'>
-          <img src="../public/assets/images/logo.png" className='h-9 rounded-full' alt="Logo" />
-          <h1 className='text-3xl text-center py-6 font-bold'>ChatGBT</h1>
+          <img src="../public/assets/images/logo.png" className='h-10 rounded-full  ' alt="Logo" />
+          <h1 className='text-[30px] text-center py-6  font-bold'>ChatGBT</h1>
         </div>
 
         <div className='flex items-center justify-start'>
-          <button className='w-full h-[50px] text-white font-[600] outline-none rounded bg-[#4f4fe2] hover:bg-slate-600'>+ New Chat</button>
+          <button className='w-full h-[50px] text-white font-[600] text-[18px] outline-none rounded bg-[#4f4fe2] hover:bg-slate-600'>+ New Chat</button>
         </div>
         <div className='h-[60%] mt-8 overflow-scroll overflow-x-hidden hide-scroll-bar shadow-lg'>
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3].map((item, index) => (
@@ -57,11 +44,11 @@ const Chat = () => {
         {chats.length > 0 ? (
           <div className='h-[80%] overflow-scroll hide-scroll-bar pt-8'>
             {chats.map((item, index) => (
-              <div key={index} className={`w-[60%] border mx-auto p-6 text-white ${item.role === 'user' ? 'bg-[#333]' : 'bg-[#555]'}`}>
+              <div key={index} className={`w-[60%] mx-auto p-4 flex text-white ${item.role === 'assistant' && 'bg-slate-900'} rounded`}>
                 <span className='text-white'>
-                  {item.role === 'user' ? <i className='bx bx-user'></i> : <i className='bx bxs-user'></i>}
+                  {item.role === 'user' ? <i className='bx bx-user-circle p-2 text-[22px]' ></i> : <img src="../public/assets/images/logo.png" className='h-[38px] p-2 rounded-full'/> }
                 </span>
-                <div>{item.message}</div>
+                <div className='w-[90%] mt-[7px]'>{item.message}</div>
               </div>
             ))}
           </div>
@@ -70,7 +57,7 @@ const Chat = () => {
             <div className='text-4xl font-bold'>APP GBT</div>
             <div className='flex flex-wrap justify-evenly py-4 max-w-[900px]'>
               {example.map((item, index) => (
-                <div key={index} className='text-lg text-[16px] mt-3 p-2 font-light min-w-[400px] border rounded cursor-pointer hover:bg-slate-600'>
+                <div key={index} className='text-lg text-[13px] mt-3 p-2 font-light min-w-[400px] border rounded cursor-pointer hover:bg-slate-600'>
                   {item}
                 </div>
               ))}
